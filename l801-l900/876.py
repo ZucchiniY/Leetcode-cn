a78eea449610fa7c8f381ccb65924fea538e5834
Zucchini <banshiliuli1990@sina.com>
2021-09-24 15:08:57 +0800

    :sparkles: 新增 876 解法

--- l801-l900/876.py
+++ l801-l900/876.py
@@ -1,8 +1,8 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+"""
+第一个解法，先计算出长度，然后做一次循环计算出中间值。
+"""
+
+
 class Solution:
     def middleNode(self, head: ListNode) -> ListNode:
         cur, l = head, 0
@@ -17,3 +17,26 @@ class Solution:
 
         curr = h
         return curr
+
+
+"""
+快慢指针法，慢指针走一步，快指针走两步，当fast 和 fast.next都存在时，继续走，如果不存在，则停止，慢指针刚好起到中间位置。
+注意需要先判断是否为空链表。
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        if head is None:
+            return None
+        slow, fast = head, head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        return slow
